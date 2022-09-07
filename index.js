ab65170f995ea073a6e8ed554a4e8f41e91100d5
Jason Bongiovanni <jpbon@protonmail.com>
2022-09-07 12:58:28 -0400

    updates

--- index.js
+++ index.js
@@ -8,9 +8,9 @@ const app = express();
 const allResults = require("./routes/allResults");
 
 app.get('/', (req, res) => {
-    res.json('Welcome to my Gaming News API, go to /routes/all to see the all news articles')
+    res.json('Welcome to my Gaming News API, go to /routes/allResults to see the all news articles')
 });
 
-app.use("/routes/all", allResults);
+app.use("/routes/allResults", allResults);
 
 app.listen(PORT, () => console.log(`server running on PORT ${PORT}`))
