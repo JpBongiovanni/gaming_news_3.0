c4e5bb9722aa9d4218fc5e2484b6e6d88769af3b
Jason Bongiovanni <jpbon@protonmail.com>
2022-09-13 12:40:03 -0400

    updates for mario route

--- index.js
+++ index.js
@@ -6,11 +6,14 @@ const cheerio = ('cheerio');
 const { get } = axios;
 const app = express();
 const allResults = require("./routes/allResults");
+const mario = require("./routes/mario");
 
 app.get('/', (req, res) => {
     res.json('Welcome to my Gaming News API, go to /routes/allResults to see the all news articles')
 });
 
 app.use("/allResults", allResults);
+
+app.use("/mario", mario);
 
 app.listen(PORT, () => console.log(`server running on PORT ${PORT}`))
