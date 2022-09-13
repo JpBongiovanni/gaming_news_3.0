3978ca105eb32a43101e6025ec80bcae664fc39b
Jason Bongiovanni <jpbon@protonmail.com>
2022-09-13 13:12:24 -0400

    updates

--- index.js
+++ index.js
@@ -5,15 +5,19 @@ const cheerio = ('cheerio');
 
 const { get } = axios;
 const app = express();
+
+//route constants
 const allResults = require("./routes/allResults");
 const mario = require("./routes/mario");
+const zelda = require("./routes/zelda");
 
 app.get('/', (req, res) => {
     res.json('Welcome to my Gaming News API, go to /routes/allResults to see the all news articles')
 });
 
+//routes URL
 app.use("/allResults", allResults);
-
 app.use("/mario", mario);
+app.use("/zelda", zelda);
 
 app.listen(PORT, () => console.log(`server running on PORT ${PORT}`))
---
+++ routes/zelda.js
@@ -0,0 +1,114 @@
+"use strict";
+const express = require('express');
+const axios = require('axios');
+const cheerio = require('cheerio');
+const { get } = axios;
+const router = express.Router();
+
+
+const sources = [
+    {
+        name: "NintendoLife",
+        address: 'https://www.nintendolife.com/',
+        base: 'https://nintendolife.com/'
+    },
+    {
+        name: "NintendoNews",
+        address: 'https://nintendonews.com/',
+        base: 'https://nintendonews.com/'
+    },
+    {
+        name: "MyNintendoNews",
+        address: "https://mynintendonews.com/",
+        base: ''
+    },
+    {
+        name: "Kotaku",
+        address: "https://kotaku.com/",
+        base: ''
+    },
+    {
+        name: "Comicbook",
+        address: "https://comicbook.com/",
+        base: ''
+    },
+    {
+        name: "GameRant",
+        address: "https://gamerant.com/",
+        base: 'https://gamerant.com/'
+    },
+    {
+        name: "NintendoEverything",
+        address: "https://nintendoeverything.com/",
+        base: ''
+    },
+    {
+        name: "Polygon",
+        address: "https://www.polygon.com/",
+        base: 'https://www.polygon.com'
+    },
+    {
+        name: "Gamespot",
+        address: "https://www.gamespot.com/",
+        base: 'https://www.gamespot.com'
+    },
+    {
+        name: "GeeksAndGamers",
+        address: "https://www.geeksandgamers.com/",
+        base: ''
+    },
+    {
+        name: "ign",
+        address: "https://www.ign.com/",
+        base: 'https://www.ign.com'
+    },
+    {
+        name: "gamesradar",
+        address: "https://www.gamesradar.com/",
+        base: ''
+    },
+    {
+        name: "PCGamer",
+        address: "https://www.pcgamer.com/",
+        base: ''
+    },
+    {
+        name: "neoseeker",
+        address: "https://www.neoseeker.com/",
+        base: ''
+    },
+    {
+        name: "giantbomb",
+        address: "https://www.giantbomb.com/",
+        base: ''
+    },
+]
+
+const zeldaArticles = [];
+
+//zelda articles
+sources.forEach(source => {
+    get(source.address)
+        .then(response => {
+            const html = response.data
+            const $ = cheerio.load(html)
+            
+            $('a:contains("Zelda"), a:contains("Ocarina of Time")', html).each(function (){
+                const text = $(this).text().trim();
+                const url = $(this).attr('href');
+
+                zeldaArticles.push({
+                    text,
+                    url: source.base + url,
+                    source: source.name,
+                    publication: source.address
+                })
+            })
+        })
+})
+
+router  
+    .get("/", async (req, res) => {res.json
+        (zeldaArticles)})
+
+module.exports = router
