3bab0d46a8cf117931ca813f39277342c34be800
Jason Bongiovanni <jpbon@protonmail.com>
2022-09-14 15:01:36 -0400

    finding error

--- index.js
+++ index.js
@@ -1,20 +1,20 @@
 const PORT = process.env.PORT || 8000;
 const express = require('express');
-const axios = require('axios');
-const cheerio = ('cheerio');
+// const axios = require('axios');
+// const cheerio = ('cheerio');
 
-const { get } = axios;
+// const { get } = axios;
 const app = express();
 
 //route constants
-const allResults = require("./routes/allResults");
-const mario = require("./routes/mario");
-const zelda = require("./routes/zelda");
-const metroid = require("./routes/metroid");
-const donkeyKong = require("./routes/donkeyKong");
-const starWars = require("./routes/starWars");
+// const allResults = require("./routes/allResults");
+// const mario = require("./routes/mario");
+// const zelda = require("./routes/zelda");
+// const metroid = require("./routes/metroid");
+// const donkeyKong = require("./routes/donkeyKong");
+// const starWars = require("./routes/starWars");
 
-app.get('/', (req, res) => {
+app.get('/', async (req, res) => {
     try {
         res.json('Welcome to my Gaming News API, go to /routes/allResults to see the all news articles')
     } catch (err) {
@@ -24,11 +24,11 @@ app.get('/', (req, res) => {
 
 // routes URL
 
-app.use("/allResults", allResults);
-app.use("/mario", mario);
-app.use("/zelda", zelda);
-app.use("/metroid", metroid);
-app.use("/donkeyKong", donkeyKong);
-app.use("/starWars", starWars);
+// app.use("/allResults", allResults);
+// app.use("/mario", mario);
+// app.use("/zelda", zelda);
+// app.use("/metroid", metroid);
+// app.use("/donkeyKong", donkeyKong);
+// app.use("/starWars", starWars);
 
 app.listen(PORT, () => console.log(`server running on PORT ${PORT}`))
