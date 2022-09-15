fd9ee80292c3af91482a0ed0099356deeb4730dd
Jason Bongiovanni <jpbon@protonmail.com>
2022-09-15 10:32:51 -0400

    try catch

--- routes/donkeyKong.js
+++ routes/donkeyKong.js
@@ -11,27 +11,32 @@ const sources = require('../sources');
 const donkeyKongArticles = [];
 
 //Donkey Kong articles
-sources.forEach(source => {
-    get(source.address)
-        .then(response => {
-            const html = response.data
-            const $ = cheerio.load(html)
-            
-            $('a:contains("Donkey Kong")', html).each(function (){
-                const text = $(this).text().trim();
-                const url = $(this).attr('href');
+try{
+    sources.forEach(source => {
+        get(source.address)
+            .then(response => {
+                const html = response.data
+                const $ = cheerio.load(html)
                 
-                donkeyKongArticles.push({
-                    text,
-                    url: source.base + url,
-                    source: source.name,
-                    publication: source.address
+                $('a:contains("Donkey Kong")', html).each(function (){
+                    const text = $(this).text().trim();
+                    const url = $(this).attr('href');
+                    
+                    donkeyKongArticles.push({
+                        text,
+                        url: source.base + url,
+                        source: source.name,
+                        publication: source.address
+                    })
                 })
             })
+    })
+} catch (err){
+    res.json("Something went wrong: " + err);
+    throw new Error(err);
+    console.log(err)
+}
 
-
-        })
-})
 
 router
     .get("/", (req, res) => {
@@ -39,6 +44,7 @@ router
             res.json(donkeyKongArticles)
         } catch (err) {
             res.json('Something went wrong: ' + err)
+            throw new Error(err)
         }
         
     })
